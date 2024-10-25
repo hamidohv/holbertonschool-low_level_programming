ee2946660cc7be1400269b07e6faf9bfb586efa0
hamidohv <hamidhamidovaga@gmail.com>
2024-10-25 15:29:22 +0000

    ifelese1

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
+/* Main function */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%i is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%i is zero\n", n);
-	}
-	else
-	{
-		printf("%i is negative\n", n);
-	}
-	return (0);
+    srand(time(0)); // Seed the random number generator
+    n = rand() - RAND_MAX / 2; // Generate a random number
+
+    /* Check if n is positive, negative, or zero */
+    if (n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else if (n == 0)
+    {
+        printf("%d is zero\n", n);
+    }
+    else
+    {
+        printf("%d is negative\n", n);
+    }
+
+    return (0); // Return success
 }
