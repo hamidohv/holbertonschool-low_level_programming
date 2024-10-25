0bed3684c5523b55d7976d6af7a39a7c30142d1d
hamidohv <hamidhamidovaga@gmail.com>
2024-10-25 15:53:22 +0000

    number

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - entry point
+ *
+ * Return: if
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
