fe4371b24c2b2b99c6e0f0bd70cc28bd18cc5e6a
hamidohv <hamidhamidovaga@gmail.com>
2024-10-25 15:56:18 +0000

    all single digit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - entry point
+ *
+ * Return: if
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
