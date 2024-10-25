d842336f9d5fa7bf3da067712ca3ccd2cd9dca2f
hamidohv <hamidhamidovaga@gmail.com>
2024-10-25 15:41:08 +0000

    small letter

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - entry point
+ *
+ * Return: if
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
