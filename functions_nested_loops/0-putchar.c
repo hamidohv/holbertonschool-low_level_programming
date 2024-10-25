812f0d2c8b5516c049f37bec01ee6ee09bbea351
hamidohv <hamidhamidovaga@gmail.com>
2024-10-25 16:26:04 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
-*main - Writes specified string.
-*
-*Return: Always 0.
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char string[] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(string[i]);
-	}
-	_putchar('\n');
-	return (0);
+    // Print _putchar followed by a new line
+    printf("_putchar\n");
+    return (0);
 }
